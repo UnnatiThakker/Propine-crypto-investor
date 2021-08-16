0242c7b8ff5cfc4ceaa8cb0cb9bcb7cbebd8fcc0
unnatithakker <unnati.thakker@yahoo.in>
2021-08-16 20:52:41 +0530

    config folder

---
+++ app/config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    DATABASE: {
+        NAME: "investor",
+        HOST: "localhost",
+        USER: "YOUR_USER_NAME",
+        PASSWORD: "YOUR_PASSWORD"
+    },
+    API: {
+        KEY: 'YOUR_API_KEY',
+        GET_RATE: 'https://min-api.cryptocompare.com/data/price'
+    }
+}
